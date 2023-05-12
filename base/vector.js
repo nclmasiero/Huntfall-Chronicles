8c80107e44dcf595418b3e8c61cffd74d0ff766f
nclmasiero <nclmasiero@gmail.com>
2023-05-12 11:54:25 +0200

    + angles handling in NclVector class

--- base/vector.js
+++ base/vector.js
@@ -1,4 +1,12 @@
 class NclVector {
+    static radiansToDegrees(radians) {
+        return (360 * radians) / (2*Math.PI);
+    }
+
+    static degreesToRadians(degrees) {
+        return (2*Math.PI * degrees) / 360;
+    }
+
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -28,6 +36,10 @@ class NclVector {
         let identityVector = new NclVector(this.x, this.y);
         identityVector.setMag(1);
         return identityVector;
+    }
+
+    getAngle() {
+        return Math.atan(this.x / this.y);
     }
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const Entity = require("./base/entity.js");
+const NclVector = require("./base/vector.js");
 const utils = require("./base/utils.js");
 
 // server setup
@@ -13,23 +14,10 @@ const server = app.listen(3000, () => {
 const io = socketio(server);
 
 // code
-var entities = [];
-var frameCounter = 0;
-
-setInterval(() => {
-    for (let entity of entities) {
-        entity.update();
-    }
-    frameCounter++;
-}, 16);
+var v = new NclVector(1, 1);
+console.log(NclVector.radiansToDegrees(v.getAngle()));
 
 // events
 io.on("connection", (socket) => {
     console.log("New connection: " + socket.id);
-    
-    entities.push(new Entity("test", utils.nclRandom(0, 800), utils.nclRandom(0, 500), 50, 50));
-
-    socket.on("getUpdate", () => {
-        socket.emit("update", entities);
-    });
 });
