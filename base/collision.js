a87e9bc2a5d1e4357fedfe3c6cbcd9bd4e3384a8
nclmasiero <nclmasiero@gmail.com>
2023-05-17 13:30:15 +0200

    + collisions!

--- base/collision.js
+++ base/collision.js
@@ -2,13 +2,21 @@ class Collision {
     static checkCollisions(entities) {
         for (let i = 0; i < entities.length; i++) {
             for(let j = i + 1; j < entities.length; j++) {
-                let mask1 = entities[0].getScript("Mask");
-                let mask2 = entities[1].getScript("Mask");
+                let mask1 = entities[i].getScript("Mask");
+                let mask2 = entities[j].getScript("Mask");
 
                 let collision = mask1.isOverlapping(mask2);
-                console.log(collision);
+                
+                if (collision != false) this.applyCollision(entities[i], entities[j], collision);
             }
         }
+    }
+
+    static applyCollision(a, b, vec) {
+        console.log(vec);
+        vec.setMag(vec.getMag() / 2);
+        a.position.add(vec);
+        b.position.sub(vec);
     }
 }
 
--- base/scripts/mask.js
+++ base/scripts/mask.js
@@ -1,4 +1,5 @@
 const Script = require("../script.js");
+const NclVector = require("../vector.js");
 
 class Mask extends Script {
     constructor() {
@@ -6,6 +7,23 @@ class Mask extends Script {
     }
 
     isOverlapping(otherMask) {
+        let r1 = Math.min(this.parent.size.x, this.parent.size.y) / 2;
+        let r2 = Math.min(otherMask.parent.size.x, otherMask.parent.size.y) / 2;
+        let rSum = r1 + r2;
+
+        let xDist = this.parent.position.x - otherMask.parent.position.x;
+        let yDist = this.parent.position.y - otherMask.parent.position.y;
+        let distance = Math.sqrt(xDist * xDist + yDist * yDist);
+
+        let collisionVector = new NclVector(this.parent.position.x, this.parent.position.y);
+        collisionVector.sub(otherMask.parent.position);
+        collisionVector.setMag(Math.abs(distance - rSum));
+
+        if (distance >= rSum) return false;
+        return collisionVector;
+    }
+
+    isOverlappingRect(otherMask) {
         let xCheck1 = this.parent.position.x + this.parent.size.x/2 > otherMask.parent.position.x - otherMask.parent.size.x/2;
         let xCheck2 = this.parent.position.x - this.parent.size.x/2 < otherMask.parent.position.x + otherMask.parent.size.x/2;
         let yCheck1 = this.parent.position.y + this.parent.size.y/2 > otherMask.parent.position.y - otherMask.parent.size.y/2;
